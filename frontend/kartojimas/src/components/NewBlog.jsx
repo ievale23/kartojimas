62f274d4b9a12600a1718f0f4ca07ea61a57cb45
ievale23 <ieva.lebedyte@gmail.com>
2023-05-12 13:19:06 +0300

    Neveikianti forma

--- frontend/kartojimas/src/components/NewBlog.jsx
+++ frontend/kartojimas/src/components/NewBlog.jsx
@@ -1,5 +1,20 @@
 import { Form } from "react-router-dom";
 
 export const NewBlog = () => {
-  return <div>Labas</div>;
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Post Image</Form.Label>
+        <Form.Control type="text" placeholder="Image url" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Post Title</Form.Label>
+        <Form.Control type="text" placeholder="Title" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Post Text</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+  );
 };
